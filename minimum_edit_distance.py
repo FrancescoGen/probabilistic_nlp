f4a1eaa7b6bfa7f7b917c3692bd4e1b3c4f14681
fran-gen <fra.fran.francesco@gmail.com>
2022-12-06 15:16:00 +0200

    added requirements.txt, text file to analyze and first function that processes the data.

--- minimum_edit_distance.py
+++ minimum_edit_distance.py
@@ -1,1 +1,37 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+from collections import Counter
+import numpy as np
+import pandas as pd
+
+
+# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: process_data
+def process_data(file_name):
+    """
+    Input:
+        A file_name which is found in your current directory. You just have to read it in.
+    Output:
+        words: a list containing all the words in the corpus (text file you read) in lower case.
+    """
+    words = []  # return this variable correctly
+
+    ### START CODE HERE ###
+    with open(file_name) as f:
+        file_name_data = f.read()
+    file_name_data = file_name_data.lower()
+    words = re.findall('\w+', file_name_data)
+    ### END CODE HERE ###
+
+    return words
+
+
+# DO NOT MODIFY THIS CELL
+word_l = process_data('/Users/macbook/Desktop/Projects/probabilistic_nlp/shakespeare.txt')
+vocab = set(word_l)  # this will be your new vocabulary
+print(f"The first ten words in the text are: \n{word_l[0:10]}")
+print(f"There are {len(vocab)} unique words in the vocabulary.")
+
 
