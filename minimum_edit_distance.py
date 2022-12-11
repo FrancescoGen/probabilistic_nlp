df871adcc8fa4205f9241addd027c001d2997721
fran-gen <fra.fran.francesco@gmail.com>
2022-12-11 17:25:34 +0200

    added a function that calculates the number of times a given word appears in the corpus.

--- minimum_edit_distance.py
+++ minimum_edit_distance.py
@@ -21,8 +21,6 @@ def process_data(file_name):
     Output:
         words: a list containing all the words in the corpus (text file you read) in lower case.
     """
-    words = []  # return this variable correctly
-
     with open(file_name) as f:
         file_name_data = f.read()
     file_name_data = file_name_data.lower()
@@ -30,11 +28,30 @@ def process_data(file_name):
 
     return words
 
-
-# DO NOT MODIFY THIS CELL
 word_l = process_data(full_path)
 vocab = set(word_l)  # this will be your new vocabulary
 print(f"The first ten words in the text are: \n{word_l[0:10]}")
 print(f"There are {len(vocab)} unique words in the vocabulary.")
 
+# A function that calculates the number of times " \
+# a word appear in the corpus.
+def get_count(word_l):
+    '''
+    Input:
+        word_l: a set of words representing the corpus.
+    Output:
+        word_count_dict: The wordcount dictionary where key is the word and value is its frequency.
+    '''
 
+    word_count_dict = {}
+    for w in word_l:
+        if w not in word_count_dict:
+            word_count_dict[w] = 1
+        else:
+            word_count_dict[w] += 1
+    return word_count_dict
+
+
+word_count_dict = get_count(word_l)
+print(f"There are {len(word_count_dict)} key values pairs")
+print(f"The count for the word 'thee' is {word_count_dict.get('thee', 0)}")
